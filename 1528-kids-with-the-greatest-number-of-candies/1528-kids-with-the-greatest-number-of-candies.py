5ffdb99ff31339cb8465e99a4779230eff66fa2a
elishevaTufik <155081962+elishevaTufik@users.noreply.github.com>
2024-12-31 23:19:49 +0200

    Time: 0 ms (100%), Space: 12.3 MB (22.37%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        max_candies = max(candies)
+    
+        result = [(candy + extraCandies) >= max_candies for candy in candies]
+        
+        return result
+            
