8559e3f648448c37ae04aa870649cac18acd81d8
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-01-16 23:40:07 +0200

    Time: 29 ms (66.84%), Space: 46.2 MB (99.82%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -3,12 +3,9 @@ class Solution {
          int n = grid.length;
         int count = 0;
 
-        // Iterate over each row and column to compare them
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 boolean isEqual = true;
-
-                // Compare row i with column j
                 for (int k = 0; k < n; k++) {
                     if (grid[i][k] != grid[k][j]) {
                         isEqual = false;
