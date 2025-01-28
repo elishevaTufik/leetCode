a14c45cb8057ad6c811842dd384085e915fcdb0d
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-01-28 16:33:45 +0200

    Time: 35 ms (16.81%), Space: 46 MB (6.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -4,19 +4,13 @@ class Solution {
         if (nums.length==1)
             return 0;
 
-        // boolean[] reachable = new boolean[nums.length];
-        // reachable[0] = true;  
-        
         int[] count = new int[nums.length];
         
         for (int i = 0; i < nums.length; i++) {
-            // if (reachable[i]) {
                 for (int j = i + 1; j <= i + nums[i] && j < nums.length; j++) {
-                    // reachable[j] = true; 
                     if(count[j]==0)
                         count[j]=count[i]+1;
                 } 
-            // }
         }
 
         return count[nums.length - 1]; 
