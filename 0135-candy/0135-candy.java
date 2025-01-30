105b52d5726e50b1d78ddd1514f25804addc571c
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-01-30 15:58:31 +0200

    Time: 3 ms (83.23%), Space: 46.3 MB (12.61%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int candy(int[] ratings) {
+        if(ratings.length==1)
+            return 1;
+
+
+        int[] candies = new int[ratings.length];
+
+        for (int i = 0; i < ratings.length; i++) {
+            candies[i] = 1;
+        }
+
+        int count=ratings.length;
+
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                count+=candies[i - 1] + 1-candies[i];
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                count+=Math.max(candies[i], candies[i + 1] + 1)-candies[i];
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        return count;
+    }
+}
