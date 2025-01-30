f673b192b2ffce4f720a3dba5647f65fe13f2338
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-01-30 15:59:48 +0200

    Time: 3 ms (83.23%), Space: 46.3 MB (12.61%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -10,22 +10,24 @@ class Solution {
             candies[i] = 1;
         }
 
-        int count=ratings.length;
+        // int count=ratings.length;
 
         for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                count+=candies[i - 1] + 1-candies[i];
+                // count+=candies[i - 1] + 1-candies[i];
                 candies[i] = candies[i - 1] + 1;
             }
         }
 
         for (int i = ratings.length - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
-                count+=Math.max(candies[i], candies[i + 1] + 1)-candies[i];
+                // count+=Math.max(candies[i], candies[i + 1] + 1)-candies[i];
                 candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
         }
-
+        int count=0;
+        for (int i=0;i<candies.length;i++)
+            count+=candies[i];
         return count;
     }
 }
