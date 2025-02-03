1023d0cc6a9a247a97bec1e0deae76c89be973b4
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-02-03 22:17:30 +0200

    Time: 0 ms (100%), Space: 41.4 MB (18.78%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cs
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cs
@@ -2,12 +2,16 @@ public class Solution {
     public int LargestAltitude(int[] gain) {
         int highestAltitude = 0;
         int currentAltitude = 0;
-
-        foreach (int g in gain)
-        {
-            currentAltitude += g;
+        int len=gain.Length;
+        for(int i=0;i<len;i++){
+            currentAltitude += gain[i];
             highestAltitude = Math.Max(highestAltitude, currentAltitude);
         }
+        // foreach (int g in gain)
+        // {
+        //     currentAltitude += g;
+        //     highestAltitude = Math.Max(highestAltitude, currentAltitude);
+        // }
 
         return highestAltitude;
     }
