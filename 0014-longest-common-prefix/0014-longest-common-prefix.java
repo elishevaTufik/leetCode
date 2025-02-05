ddbb623acd8426710142ad26f7fad97b8fe1d24d
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-02-05 23:16:22 +0200

    Time: 0 ms (100%), Space: 41.6 MB (63.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -9,10 +9,6 @@ class Solution {
         for(int i=1;i<strs.length;i++){
             while (strs[i].indexOf(res) != 0) {
                 res = res.substring(0, res.length() - 1);
-                
-                // if (res.isEmpty()) {
-                //     return "";
-                // }
             }
         }
         return res;
