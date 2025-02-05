231599d40e24787da83243d8ac3f26bb113d5e1e
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-02-05 23:06:51 +0200

    Time: 0 ms (100%), Space: 41.8 MB (48.49%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                while( i>=0 && s.charAt(i--)!=' ')
+                    count++;
+
+                return count;
+            }
+        }
+        return count;
+
+    }
+}
