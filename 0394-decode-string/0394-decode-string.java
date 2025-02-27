f5da576189476142aca91560e1edff764e7a736c
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-02-27 20:38:17 +0200

    Time: 0 ms (100%), Space: 41.5 MB (81.86%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String decodeString(String s) {
+       Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int count = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                count = count * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(count);
+                stringStack.push(currentString);
+                count = 0;
+                currentString = new StringBuilder();
+            } else if (ch == ']') {
+                int repeatTimes = countStack.pop();
+                StringBuilder decodedString = stringStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
+        }
+        return currentString.toString();  
+        
+    }
+}
