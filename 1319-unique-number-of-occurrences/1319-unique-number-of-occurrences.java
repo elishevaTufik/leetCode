5e358793abf1edef5a0f0e6ab0b1623cc4e29f61
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-05-27 22:14:15 +0300

    Time: 2 ms (98.59%), Space: 42.1 MB (45.52%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,26 +1,25 @@
+import java.util.*;
 class Solution {
+
+
     public boolean uniqueOccurrences(int[] arr) {
-       Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            int key = arr[i];
-            if (frequencyMap.containsKey(key)) {
-                frequencyMap.put(key, frequencyMap.get(key) + 1);
-            } else {
-                frequencyMap.put(key, 1);
-            }
-        }
+        Map<Integer,Integer> hashMap=new HashMap<>();
         
-        // בודק אם כל התדירויות הן ייחודיות
-        Set<Integer> occurrenceSet = new HashSet<>();
-        Iterator<Integer> it = frequencyMap.keySet().iterator();
-        while (it.hasNext()) {
-            int key = it.next();
-            int count = frequencyMap.get(key);
-            // אם לא ניתן להוסיף את התדירות למערך, משמעות הדבר שיש כפילות
-            if (!occurrenceSet.add(count)) {
-                return false;
+        for (int i=0; i<arr.length; i++){
+            if(hashMap.containsKey(arr[i])) {
+            	hashMap.put(arr[i],hashMap.get(arr[i])+1);
+            }
+            else {
+            	hashMap.put(arr[i],1);
             }
         }
-        return true;
+
+        Collection<Integer> c=hashMap.values();
+        Set<Integer> set = new HashSet<>(c);
+
+        if(c.size()!=set.size())
+        	return false;
+        
+        return true;       
     }
 }
