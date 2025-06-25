3652105d1cdad03da32d2d7827c6bcfe8aa7b484
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-06-25 21:04:46 +0300

    Time: 2 ms (98.53%), Space: 42.4 MB (15.49%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,25 +1,18 @@
-import java.util.*;
 class Solution {
-
-
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer> hashMap=new HashMap<>();
+       Map<Integer, Integer> countMap = new HashMap<>();
+        for (int i=0;i<arr.length;i++) {
+            countMap.put(arr[i], countMap.getOrDefault(arr[i], 0) + 1);
+        }
+
+        Set<Integer> occurrenceSet = new HashSet<>();
         
-        for (int i=0; i<arr.length; i++){
-            if(hashMap.containsKey(arr[i])) {
-            	hashMap.put(arr[i],hashMap.get(arr[i])+1);
-            }
-            else {
-            	hashMap.put(arr[i],1);
+        for (int count : countMap.values()) {
+            if (!occurrenceSet.add(count)) {
+                return false;
             }
         }
 
-        Collection<Integer> c=hashMap.values();
-        Set<Integer> set = new HashSet<>(c);
-
-        if(c.size()!=set.size())
-        	return false;
-        
-        return true;       
+        return true; 
     }
 }
