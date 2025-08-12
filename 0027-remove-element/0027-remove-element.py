9da43838257f941b6c9fdb403c76067e4a273e1e
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-08-13 01:40:34 +0300

    Update 0027-remove-element.py

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
         k=0
         for i in nums:
             if i!=val:
