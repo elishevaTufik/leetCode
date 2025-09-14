51c4bd2f8e4b7570355bbc6fd0dc121b7c8feb4b
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-09-14 15:15:36 +0300

    Time: 2 ms (96.79%), Space: 41.7 MB (87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,39 +1,36 @@
+// import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
-        for(int i=0;i<s.length();i++){
-            switch(s.charAt(i)){
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            switch (ch) {
                 case '(':
                 case '{':
                 case '[':
-                    stack.push(s.charAt(i));
+                    stack.push(ch);
                     break;
-                
-                case ')':  
-                if(stack.isEmpty()) 
-                        return false;
-                    if(stack.pop()!='(')
-                        return false;
-                        break;
+
+                case ')':
+                    if (!checkMatch(stack, '(')) return false;
+                    break;
+
                 case '}':
-                    if(stack.isEmpty()) 
-                        return false;  
-                    if(stack.pop()!='{')
-                        return false;
+                    if (!checkMatch(stack, '{')) return false;
                     break;
-                case ']':  
-                    if(stack.isEmpty()) 
-                        return false;
-                    if(stack.pop()!='[')
-                        return false;
+
+                case ']':
+                    if (!checkMatch(stack, '[')) return false;
                     break;
+
                 default:
                     return false;
             }
         }
-        if(stack.isEmpty()) 
-            return true;
-        return false;
+        return stack.isEmpty();
+    }
+
+    public boolean checkMatch(Stack<Character> stack, char expected) {
+        return !stack.isEmpty() && stack.pop() == expected;
     }
 }
