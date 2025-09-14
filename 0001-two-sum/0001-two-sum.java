4fc2c7873a70748de821d2dc3523df381621f9b2
Elisheva Tufik <155081962+elishevaTufik@users.noreply.github.com>
2025-09-14 14:41:18 +0300

    Time: 2 ms (98.9%), Space: 45.2 MB (18.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,12 @@
-import java.util.HashMap;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<nums.length; i++){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
             if(map.containsKey(target-nums[i]))
-            {
-                return new int[] { map.get(target-nums[i]), i };
-            }
-                
+                return new int[]{map.get(target-nums[i]),i};
             map.put(nums[i],i);
         }
-        throw new IllegalArgumentException("No two sum solution found");
+        return new int[]{-1,-1};
     }
 }
